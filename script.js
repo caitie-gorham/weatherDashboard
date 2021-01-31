bfdd8cff41e05b5499aa99917cb58abf1d31f879
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-30 18:32:13 -0600

    updated script to have weather icons in each card showing weather

--- script.js
+++ script.js
@@ -16,9 +16,14 @@ $(document).ready(function() {
             method: "GET"
         }).then(function(response){
             // get one day from now
+            console.log(response)
             let dtText1 = response.list[0].dt_txt;
             let dtText1Loc = new Date(dtText1).toLocaleDateString()
             $("#date-input-1").text(dtText1Loc)
+            let iconInput1 = response.list[0].weather[0].icon
+            iconURL1 = "http://openweathermap.org/img/w/" + iconInput1 + ".png"
+            var img = $("<img>").attr("src", iconURL1);
+            $("#card-1").append(img)
             let tempInput1 = response.list[0].main.temp
             $("#temp-input-1").text("Temperature: " + tempInput1 + " °F")
             let humInput1 = response.list[0].main.humidity
@@ -28,6 +33,10 @@ $(document).ready(function() {
             let dtText2 = response.list[7].dt_txt;
             let dtText2Loc = new Date(dtText2).toLocaleDateString();
             $("#date-input-2").text(dtText2Loc)
+            let iconInput2 = response.list[7].weather[0].icon
+            iconURL2 = "http://openweathermap.org/img/w/" + iconInput2 + ".png"
+            var img = $("<img>").attr("src", iconURL2);
+            $("#card-2").append(img)
             let tempInput2 = response.list[7].main.temp
             $("#temp-input-2").text("Temperature: " + tempInput2 + " °F")
             let humInput2 = response.list[7].main.humidity
@@ -37,6 +46,10 @@ $(document).ready(function() {
             let dtText3 = response.list[15].dt_txt;
             let dtText3Loc = new Date(dtText3).toLocaleDateString()
             $("#date-input-3").text(dtText3Loc)
+            let iconInput3 = response.list[15].weather[0].icon
+            iconURL3 = "http://openweathermap.org/img/w/" + iconInput3 + ".png"
+            var img = $("<img>").attr("src", iconURL3);
+            $("#card-3").append(img)
             let tempInput3 = response.list[15].main.temp
             $("#temp-input-3").text("Temperature: " + tempInput3 + " °F")
             let humInput3 = response.list[15].main.humidity
@@ -46,6 +59,10 @@ $(document).ready(function() {
             let dtText4 = response.list[23].dt_txt;
             let dtText4Loc = new Date(dtText4).toLocaleDateString();
             $("#date-input-4").text(dtText4Loc)
+            let iconInput4 = response.list[23].weather[0].icon
+            iconURL4 = "http://openweathermap.org/img/w/" + iconInput4 + ".png"
+            var img = $("<img>").attr("src", iconURL4);
+            $("#card-4").append(img)
             let tempInput4 = response.list[23].main.temp
             $("#temp-input-4").text("Temperature: " + tempInput4 + " °F")
             let humInput4 = response.list[23].main.humidity
@@ -55,6 +72,10 @@ $(document).ready(function() {
             let dtText5 = response.list[31].dt_txt;
             let dtText5Loc = new Date(dtText5).toLocaleDateString()
             $("#date-input-5").text(dtText5Loc)
+            let iconInput5 = response.list[31].weather[0].icon
+            iconURL5 = "http://openweathermap.org/img/w/" + iconInput5 + ".png"
+            var img = $("<img>").attr("src", iconURL5);
+            $("#card-5").append(img)
             let tempInput5 = response.list[31].main.temp
             $("#temp-input-5").text("Temperature: " + tempInput5 + " °F")
             let humInput5 = response.list[31].main.humidity
@@ -77,6 +98,10 @@ $(document).ready(function() {
             let searchSpeed = response.wind.speed;
             let localTime = luxon.DateTime.local();
             let formatTime = localTime.toLocaleString(luxon.DateTime.DATE_SHORT);
+            let weatherIconCurrent = response.weather[0].icon
+            iconURLCurrent = "http://openweathermap.org/img/w/" + weatherIconCurrent + ".png"
+            var imgCurrent = $("<img>").attr("src", iconURLCurrent);
+            $("#current-cont").prepend(imgCurrent)
             $("#city-date").text(searchName + " (" + formatTime + ")");
             $("#temperature").text("Temperature: " + searchTemp + " °F");
             $("#humidity").text("Humidity: " + searchHumidity + "%");
