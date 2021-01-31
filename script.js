66cce770b6935c1d40036a8b5ef00ce36ce97c05
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-30 19:13:29 -0600

    functionality that creates UV index section and appends it to the current weather div

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     // get array stored in local storage
     var historyList = JSON.parse(window.localStorage.getItem("historyList")) || [];
     // and create a list using the values
-    for (i=0; i<historyList.length; i++) {
+    for (i = 0; i < historyList.length; i++) {
         $(".history-group").append('<a href="#" class="list-group-item list-group-item-action history">' + historyList[i] + '</a>');
     }
 
     // click event that generates the five day forecast for searched city
-    $("#searchBtn").on("click", function(e){
+    $("#searchBtn").on("click", function (e) {
         e.preventDefault();
         let cityInput = $("#city-input").val()
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=8755afb2d2e1bf2924f3c5f7af0776c4&units=imperial",
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             // get one day from now
             console.log(response)
             let dtText1 = response.list[0].dt_txt;
@@ -28,7 +28,7 @@ $(document).ready(function() {
             $("#temp-input-1").text("Temperature: " + tempInput1 + " °F")
             let humInput1 = response.list[0].main.humidity
             $("#humidity-input-1").text("Humidity: " + humInput1 + "%")
-            
+
             // get two days from now
             let dtText2 = response.list[7].dt_txt;
             let dtText2Loc = new Date(dtText2).toLocaleDateString();
@@ -54,7 +54,7 @@ $(document).ready(function() {
             $("#temp-input-3").text("Temperature: " + tempInput3 + " °F")
             let humInput3 = response.list[15].main.humidity
             $("#humidity-input-3").text("Humidity: " + humInput3 + "%")
-            
+
             // get four days from now
             let dtText4 = response.list[23].dt_txt;
             let dtText4Loc = new Date(dtText4).toLocaleDateString();
@@ -67,7 +67,7 @@ $(document).ready(function() {
             $("#temp-input-4").text("Temperature: " + tempInput4 + " °F")
             let humInput4 = response.list[23].main.humidity
             $("#humidity-input-4").text("Humidity: " + humInput4 + "%")
-            
+
             // get five days from now
             let dtText5 = response.list[31].dt_txt;
             let dtText5Loc = new Date(dtText5).toLocaleDateString()
@@ -84,13 +84,13 @@ $(document).ready(function() {
     })
 
     // click event that generates the current weather for searched city
-    $("#searchBtn").on("click", function(e){
+    $("#searchBtn").on("click", function (e) {
         e.preventDefault()
         let cityInput = $("#city-input").val()
         $.ajax({
             url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=8755afb2d2e1bf2924f3c5f7af0776c4&units=imperial",
             method: "GET"
-        }).then(function(response){
+        }).then(function (response) {
             console.log(response)
             let searchName = response.name;
             let searchTemp = response.main.temp;
@@ -106,21 +106,42 @@ $(document).ready(function() {
             $("#temperature").text("Temperature: " + searchTemp + " °F");
             $("#humidity").text("Humidity: " + searchHumidity + "%");
             $("#wind-speed").text("Wind Speed: " + searchSpeed + "MPH");
+            uvCall(response.coord.lat, response.coord.lon)
         })
     })
 
-    $("#searchBtn").on("click", function(){
+    $("#searchBtn").on("click", function () {
         let cityInput = $("#city-input").val();
-        if (historyList.indexOf(cityInput)=== -1) {
+        if (historyList.indexOf(cityInput) === -1) {
             historyList.push(cityInput);
             window.localStorage.setItem("historyList", JSON.stringify(historyList));
         }
     })
 
     // create a click event for each item in the list that fills in the search input
-    $(".history").on("click", function(){
+    $(".history").on("click", function () {
         $("#city-input").val($(this).text())
     });
+
+    function uvCall(lat, long) {
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/uvi?appid=8755afb2d2e1bf2924f3c5f7af0776c4&lat=" + lat + "&lon=" + long,
+            method: "GET"
+        }).then(function (response) {
+            let uvValue = $("<p>").text("UV: ");
+            let toggle = $("<span>").addClass("btn btn-sm").text(response.value);
+            if (response.value < 3) {
+                toggle.addClass("btn-success");
+            }
+            else if (response.value < 7) {
+                toggle.addClass("btn-warning");
+            }
+            else {
+                toggle.addClass("btn-danger");
+            }
+            $("#current-cont").append(uvValue.append(toggle))
+        })
+    }
 
 })
 
